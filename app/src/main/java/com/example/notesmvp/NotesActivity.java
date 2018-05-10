1dd1746128d16fbfd8866236e0a63edf77b4b229
Javier Aranda <javierarandacaro@gmail.com>
2018-05-10 23:08:32 +0200

    RecyclerView Notes created.

--- app/src/main/java/com/example/notesmvp/NotesActivity.java
+++ app/src/main/java/com/example/notesmvp/NotesActivity.java
@@ -2,12 +2,18 @@ package com.example.notesmvp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 public class NotesActivity extends AppCompatActivity {
-
+    private Toolbar toolbar;
+    private RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.tbNotesActivity);
+        setSupportActionBar(toolbar);
     }
 }
